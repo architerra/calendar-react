9fa71a625ad3db5f90a52817e78ade4a2e503c9b
Olexiy Voedylo <architerr@gmail.com>
2023-04-04 16:33:49 +0300

    ch1

---
+++ src/components/calendar/Calendar.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+import Navigation from './../navigation/Navigation';
+import Week from '../week/Week';
+import Sidebar from '../sidebar/Sidebar';
+import events from '../../gateway/events';
+
+import './calendar.scss';
+
+const Calendar = ({ weekDates }) => {
+  return (
+    <section className="calendar">
+      <Navigation weekDates={weekDates} />
+      <div className="calendar__body">
+        <div className="calendar__week-container">
+          <Sidebar />
+          <Week weekDates={weekDates} events={events} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Calendar;
